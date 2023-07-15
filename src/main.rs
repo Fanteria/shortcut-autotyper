3101645b4d7d6b1413e5ef19dc8f662e3a7d379f
Jiří Alexandrovič <jiri.alexandrovic@gmail.com>
2023-07-15 22:11:34 +0200

    Initial code commit.

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![allow(dead_code, unused_variables)]
+
+mod error;
+mod sequence;
+mod combinations;
+
+// use crate::sequence::Sequences;
+// use enigo::{Enigo, KeyboardControllable};
+
+fn main() {
+
+
+    // let mut enigo = Enigo::new();
+    // // write text
+    // enigo.key_sequence("Hello World! here is a lot of text  ❤️" );
+
+    // select all
+    // enigo.key_down(Key::Control);
+    // enigo.key_click(Key::Layout('a'));
+    // enigo.key_up(Key::Control);
+}
